3be502a9b1a460f2cc2f96d73838b9f722c33e81
isaiahaquino <isaiahaquino1997@gmail.com>
2024-09-13 21:25:03 -0700

    hero

--- app/page.js
+++ app/page.js
@@ -26,7 +26,9 @@ export default function Homepage() {
       </div>
 
       {/* Hero */}
-      <div className="mx-auto max-w-7xl px-6 lg:px-8 my-36 flex items-center">
+      <div className="relative isolate overflow-hidden mx-auto max-w-7xl px-6 lg:px-8 py-36 flex items-center">
+        <div className="absolute inset-0 -z-10 bg-[radial-gradient(35rem_40rem_at_top,theme(colors.primary),white)] opacity-20" />
+        <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-primary-600/10 ring-primary-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
         <div className="w-full flex flex-col my-auto gap-8">
           <h2 className="text-4xl font-bold text-black sm:text-6xl">Introducing<br />High Tide AI</h2>
           <p className="tracking-wide">Every single time we use our Earth's most precious resource, we must be mindful of our usage. Our mobile app helps monitor, detect and alert millions of water users about how much water you save and earn rewards!</p>
