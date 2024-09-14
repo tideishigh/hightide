08c83581d59382127970d247da7b88abfa73103d
isaiahaquino <isaiahaquino1997@gmail.com>
2024-09-13 22:06:24 -0700

    header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {
   Dialog,
   DialogPanel,
@@ -13,14 +13,70 @@ import {
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [nav, setNav] = useState('fixed')
+  const [scrollTop, setScrollTop] = useState(0)
+  const [oldTop, setOldTop] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = (event) => {
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+  
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (oldTop >= scrollTop) {
+      // scrolling up
+      setNav('opacity-100 translate-y-4')
+    } else {
+      // scrolling down
+      setNav('opacity-0 z-[-1]')
+    }
+    setOldTop(scrollTop)
+  }, [scrollTop])
+
+  useEffect(() => {
+    const reveal = () => {
+      var reveals = document.querySelectorAll(".reveal")
+      for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight
+        var elementTop = reveals[i].getBoundingClientRect().top
+        var elementVisible = 200
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active")
+          Array.from(reveals[i].children).forEach(child => {
+            // setTimeout(() => {
+            //   child.classList.add("reveal")
+            //   child.classList.add("active")
+            // }, 500)
+          })
+        } 
+        // else {
+        //   reveals[i].classList.remove("active")
+        // }
+      }
+    }
+
+    window.addEventListener("scroll", reveal)
+    reveal()
+
+    return () => {
+      window.removeEventListener("scroll", reveal)
+    }
+  }, [])
 
   return (
-    <header className="bg-white bg-opacity-80 absolute z-10 w-full">
+    <header className={`bg-white bg-opacity-80 fixed z-10 w-full top-[-1rem] ${nav} ${scrollTop == 0 ? 'shadow-none bg-opacity-0' : 'shadow-lg'} transition-all duration-300 ease-in-out`}>
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-1 lg:px-3">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Hightide</span>
-            <img alt="Hightide" src="/hightide_logo.png" className="h-12 w-auto" />
+            <img alt="Hightide" src={`${scrollTop == 0 ? "/hightide_logo_white.png" : "/hightide_logo.png"}`} className="h-10 p-1 w-auto" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -30,17 +86,17 @@ export default function Header() {
             className=" inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            <Bars3Icon aria-hidden="true" className={`h-6 w-6 ${scrollTop == 0 ? "text-white" : "text-black"}`} />
           </button>
         </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm font-medium leading-6 text-black">
+        <PopoverGroup className="hidden lg:flex lg:gap-x-4">
+          <a href="#" className={`text-sm font-semibold leading-6 hover:bg-black hover:bg-opacity-20 py-1 px-3 rounded ${scrollTop == 0 ? "text-white" : "text-black"}`}>
             About
           </a>
-          <a href="#" className="text-sm font-medium leading-6 text-black">
+          <a href="#" className={`text-sm font-semibold leading-6 hover:bg-black hover:bg-opacity-20 py-1 px-3 rounded ${scrollTop == 0 ? "text-white" : "text-black"}`}>
             Features
           </a>
-          <a href="#" className="text-sm font-medium leading-6 text-black">
+          <a href="#" className={`text-sm font-semibold leading-6 hover:bg-black hover:bg-opacity-20 py-1 px-3 rounded ${scrollTop == 0 ? "text-white" : "text-black"}`}>
             Company
           </a>
         </PopoverGroup>
