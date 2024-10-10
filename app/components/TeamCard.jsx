b6fd40c71d3a994490e6c3e3050c3f5da63fa192
isaiahaquino <isaiahaquino1997@gmail.com>
2024-10-10 12:07:24 -1000

    default team headshot

--- app/components/TeamCard.jsx
+++ app/components/TeamCard.jsx
@@ -3,14 +3,15 @@ import Image from "next/image"
 export default function TeamCard({ name, title, imgSrc}) {
   return (
     <div className="w-[230px]">
-      <Image
-        alt=""
-        src={imgSrc}
-        height="230"
-        width="230"
-        objectFit="cover"
-        className="rounded-2xl mb-6"
-      />
+      <div className="h-[230px] w-[230px] mb-6 relative">
+        <Image
+          alt={name}
+          src={imgSrc}
+          fill
+          objectFit="cover"
+          className="rounded-2xl"
+        />
+      </div>
       <h4 className="text-xl font-semibold">{name}</h4>
       <p className="text-neutral">{title}</p>
     </div>
